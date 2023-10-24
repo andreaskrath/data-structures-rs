21f8458216d323f5fc6ba9aa182dfa32cba22eec
Andreas Krath <andreas.krath+github@gmail.com>
2023-10-24 22:37:35 +0200

    document item methods

--- src/shared.rs
+++ src/shared.rs
@@ -14,14 +14,17 @@ impl<T: PartialOrd> Item<T> {
         }
     }
 
+    /// Returns a reference to the value of the item.
     pub fn value(&self) -> &T {
         &self.value
     }
 
+    /// Returns an `Option` containing a mutable reference to the left child of the item.
     pub fn left(&mut self) -> Option<&mut Self> {
         self.left.as_deref_mut()
     }
 
+    /// Returns an `Option` containing a mutable reference to the right child of the item.
     pub fn right(&mut self) -> Option<&mut Self> {
         self.right.as_deref_mut()
     }

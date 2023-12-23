11bc3ce8fcf7945578d8dcb7d6acea13dff37cf9
Andreas Krath <andreas.krath+github@gmail.com>
2023-12-23 20:51:15 +0100

    dev: add nix-shell to repository

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> { overlays = [ (import <rust-overlay>) ]; } }:
+let
+  rust = pkgs.rust-bin.stable.latest.default.override {
+    extensions = [ "rust-src" ];
+  };
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    rust
+    just
+  ];
+}
